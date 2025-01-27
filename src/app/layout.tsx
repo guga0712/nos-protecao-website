139c07d3307a216e9692f26f16d2566ec9cd65de
guga0712 <martins0712@hotmail.com>
2025-01-27 17:30:49 -0300

    feat: add meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,40 @@
 // app/layout.tsx
 import { Providers } from "./providers";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Nós Proteção | Segurança e Qualidade para Sua Carga",
+  description:
+    "A Nós Proteção oferece as melhores soluções para embalar, proteger e transportar sua carga com segurança. Filmes Stretch, Lonas Plásticas, Madeirites e muito mais! Proteja seus produtos com qualidade e resistência.",
+  keywords: [
+    "proteção de carga",
+    "embalagem de carga",
+    "filme stretch",
+    "lonas plásticas",
+    "madeirites",
+    "segurança no transporte",
+    "embalagens resistentes",
+    "logística segura",
+  ],
+  authors: [{ name: "Nós Proteção" }],
+  robots: "index, follow",
+  openGraph: {
+    title: "Nós Proteção | Segurança e Qualidade para Sua Carga",
+    description:
+      "Embalagens e proteções de carga com máxima resistência e durabilidade. Confira nossa linha completa de Filmes Stretch, Lonas Plásticas, Madeirites e muito mais!",
+    url: "http://nosprotecaodecarga.com.br/",
+    siteName: "Nós Proteção",
+    images: [
+      {
+        url: "http://nosprotecaodecarga.com.br/logo-orange.jpg",
+        width: 500,
+        height: 500,
+        alt: "Proteção de carga com segurança e resistência",
+      },
+    ],
+    type: "website",
+  },
+};
 
 export default function RootLayout({
   children,
